8c6f0e3941f6900a8e721be68a3661a1eb9aca43
abanerjee-06 <aritra.b2000@outlook.com>
2021-04-02 15:32:43 +0300

    Storing results in required format

--- 2018102006_A1/Programming Assignment 1/Q2.py
+++ 2018102006_A1/Programming Assignment 1/Q2.py
@@ -31,28 +31,27 @@ count = 1
 cuts = [0]
 
 print("Length of M.AaaS1ORF662P gene = ",l,"bp\n")
-while (end1-l < 6):
-    if (end1 <= l):
-        site1 = DNA_seq[start1:end1:] 
-        if (site1[1]=='A' and site1[2]=='A' and site1[3]=='T' and site1[4]=='T' and (site1[0]=='A' or site1[0]=='G') and (site1[5]=='C' or site1[5]=='T')):           
-            print("Site No.: ",count,"\tat ",start1+1," - ",end1,"\tRE Site: ",site1,"\tRE Name: ApoI \t cuts after ",start1+1)
-            cuts.append(start1+1)
-            count += 1
-    else:
-        site1 = DNA_seq[start1:l:]+DNA_seq[0:end1-l:]
-        if (site1[1]=='A' and site1[2]=='A' and site1[3]=='T' and site1[4]=='T' and (site1[0]=='A' or site1[0]=='G') and (site1[5]=='C' or site1[5]=='T')):           
-            print("Site No.: ",count,"\tat ",start1+1," - ",end1-l,"\tRE Site: ",site1,"\tRE Name: ApoI")
-            cuts.append(start1+1)
-            count += 1
-
+while (end1 <= l):
+    site1 = DNA_seq[start1:end1:] 
+    if (site1[1]=='A' and site1[2]=='A' and site1[3]=='T' and site1[4]=='T' and (site1[0]=='A' or site1[0]=='G') and (site1[5]=='C' or site1[5]=='T')):           
+        print("Site No.: ",count,"\tat ",start1+1," - ",end1,"\tRE Site: ",site1,"\tRE Name: ApoI \t cuts after ",start1+1)
+        cuts.append(start1+1)
+        count += 1
     start1 += 1
     end1 += 1
 
+f = open("Q2_map.txt","w")
 for i in range(1,count):
     print("Fragment No.: ",i,"\tof length ",cuts[i]-cuts[i-1],"bp")
+    s = "Fragment No.: "+str(i)+"\t"+str(cuts[i-1]+1)+"-"+str(cuts[i])+"\tof length "+str(cuts[i]-cuts[i-1])+"bp\n"
+    f.write(s)
     cuts[i-1] = cuts[i] - cuts[i-1]
 print("Fragment No.: ",count,"\tof length ",l-cuts[-1],"bp")
+s = "Fragment No.: "+str(count)+"\t"+str(cuts[-1]+1)+"-"+str(l)+"\tof length "+str(l-cuts[-1])+"bp\n"
+f.write(s)
 cuts[-1] = l-cuts[-1]
+
+f.close()
 
 plt.figure(figsize=(3,10))
 colors = ['#eb3434','orange','#b5561f','#96790f','#0f593f','brown','#750970']
